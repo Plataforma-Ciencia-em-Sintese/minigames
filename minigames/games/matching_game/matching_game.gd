8cf6963a376f4b30de2fd170b985ff8dc6eb2322
rafaelsescato <rafaelsescato@gmail.com>
2023-03-30 10:35:00 -0300

    FIXED(Matching Game): removed old images how_to_play screen

--- minigames/games/matching_game/matching_game.gd
+++ minigames/games/matching_game/matching_game.gd
@@ -23,9 +23,6 @@ const GAME_RESULTS: PackedScene = preload("res://game_results/game_results.tscn"
 const HOW_TO_PLAY: PackedScene = preload("res://how_to_play/how_to_play.tscn")
 const HOW_TO_PLAY_TEXTURES: Array = Array([
 	preload("res://assets/images/htp_matching_game_0.png"),
-	preload("res://assets/images/htp_matching_game_1.png"),
-	preload("res://assets/images/htp_matching_game_2.png"),
-	preload("res://assets/images/htp_matching_game_3.png")
 ])
 
 
