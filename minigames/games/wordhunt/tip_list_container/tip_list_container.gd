7352321880ec6f2f97e3f10d4cbce0727f698a33
rafaelsescato <rafaelsescato@gmail.com>
2023-07-29 15:09:46 -0300

    ADDED(wordhunt): tip list scene

---
+++ minigames/games/wordhunt/tip_list_container/tip_list_container.gd
@@ -0,0 +1,26 @@
+extends ScrollContainer
+
+
+const _TipListItem: PackedScene = preload("res://games/wordhunt/tip_list_item/tip_list_item.tscn")
+
+
+onready var _list: VBoxContainer = $"%List"
+
+
+func _ready() -> void:
+	insert_tips_into_tip_list(API.game.get_words())
+
+
+func insert_tips_into_tip_list(tips: Dictionary) -> void:
+	_delete_all_childs(_list)
+	for i in range(0, tips.size()):
+		var new_tip: MarginContainer = _TipListItem.instance()
+		_list.add_child(new_tip)
+		new_tip.set_word(str(tips.keys()[i]))
+		new_tip.set_tip_text(str(tips.values()[i]))
+
+
+func _delete_all_childs(node: Node) -> void:
+	for child in node.get_children():
+		node.remove_child(child)
+		child.queue_free()
